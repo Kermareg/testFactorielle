f42900c1a298c757e942accbd016566873852950
Bertrand MICHAUX <bmichaux@diouxx.be>
2015-10-16 15:54:36 +0200

    Creation factorielle via recursion

---
+++ src/Factorielle.java
@@ -0,0 +1,24 @@
+
+public class Factorielle
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Calcul factorielle");
+		System.out.println("Factorielle 5 : " + factorielle(5));
+		System.exit(0);
+	}
+	
+	public static long factorielle(long i)
+	{
+		if(i == 1)
+			return 1;
+		else
+			return i * factorielle(i-1);
+	}
+
+}
